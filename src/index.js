2d1a5d60fdba0204988b7e54ecade98cd5a92a68
blkclct <paradox.h129@gmail.com>
2020-01-14 08:26:00 +0900

    Update: the postion directory of Components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './components/App';
 
 const store = createStore(function(state, action) {
   const _state = state == null ? {
